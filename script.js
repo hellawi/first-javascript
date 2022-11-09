787a20e111cf790e17aceaf0df770789d62ab435
Michael <55346444+hellawi@users.noreply.github.com>
2022-11-08 20:20:26 -0800

    added first js

---
+++ script.js
@@ -0,0 +1,30 @@
+// alert("Are you sure to open site?");
+
+console.log("Today is 8 November")
+
+"use strict"
+var ukraine = "glory"
+const victory = 100
+let kherson = "ukraine"
+
+console.log(ukraine, victory, kherson)
+
+if(5 > 0){
+    console.log("Glory to Ukraine")
+}
+
+if(3 < 4){
+    var g = 5;
+    var b = 11;
+    var c = 26;
+}
+console.log(g+2, b, c)
+
+function background(){
+    document.body.style.background = "green"
+}
+background()
+
+a = 5
+console.log(a)
+var a 
